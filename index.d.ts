446e749074581669c3813f5a2bf42b1f0885e3d7
Max Anurin <maxim.anurin@zxteam.org>
2019-03-20 15:06:44 +0200

    Strict task's then and catch signature

--- index.d.ts
+++ index.d.ts
@@ -122,14 +122,14 @@ export interface Task<T = void> extends Promise<T> {
 	 * @param onrejected The callback to execute when the Promise is rejected.
 	 * @returns A Promise for the completion of which ever callback is executed.
 	 */
-	then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>;
+	then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: Error) => TResult2 | PromiseLike<TResult2>) | undefined | null): Task<TResult1 | TResult2>;
 
 	/**
 	 * Attaches a callback for only the rejection of the Promise.
 	 * @param onrejected The callback to execute when the Promise is rejected.
 	 * @returns A Promise for the completion of the callback.
 	 */
-	catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): Promise<T | TResult>;
+	catch<TResult = never>(onrejected?: ((reason: Error) => TResult | PromiseLike<TResult>) | undefined | null): Task<T | TResult>;
 }
 
 // export declare namespace collections {
