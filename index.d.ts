481e39713ebc1f0afeff5f3f6d1bc8846e6cbcf0
Max Anurin <maxim.anurin@zxteam.org>
2020-06-04 00:18:12 +0300

    Configuration: add getIndexer(), change keys() as getter, deprecate getConfiguration().

--- index.d.ts
+++ index.d.ts
@@ -7,19 +7,24 @@ export interface CancellationToken {
 
 export interface Configuration {
 	readonly configurationNamespace: string;
+	readonly keys: ReadonlyArray<string>;
 	get(key: string): boolean | number | string;
 	getBase64(key: string, defaultValue?: Uint8Array): Uint8Array;
 	getBoolean(key: string, defaultValue?: boolean): boolean;
+	getNamespace(configurationNamespace: string): Configuration;
+	/**
+	 * @deprecated Use `getNamespace` instead
+	 */
 	getConfiguration(configurationNamespace: string): Configuration;
 	getEnabled(key: string, defaultValue?: boolean): boolean;
 	getFloat(key: string, defaultValue?: number): number;
+	getIndexer(indexerName?: string): Array<Configuration>;
 	getInteger(key: string, defaultValue?: number): number;
 	getString(key: string, defaultValue?: string): string;
 	getURL(key: string, defaultValue?: URL): URL;
 	hasNamespace(configurationNamespace: string): boolean;
 	has(key: string): boolean;
 	hasNonEmpty(key: string): boolean;
-	keys(): ReadonlyArray<string>;
 }
 
 export interface Disposable {
